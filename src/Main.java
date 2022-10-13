b5723c0a96dd52e3399b040ed1091079d772603f
ikeaosman <ikhr0003@stud.kea.dk>
2022-10-13 11:20:52 +0200

    First commit

---
+++ src/Main.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+/*
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hej");
+
+              System.out.println("Verden");
+        System.out.println("Har i det godt?");
+
+
+    }
+*/
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        //attributter
+        String navn;
+        String superkraft;
+        double årstal;
+        boolean menneske;
+
+
+
+
+
+    }
+
+}
+
